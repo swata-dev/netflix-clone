2fadd2dfa5bc867021f8d09356bc5909aad23e0d
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-21 15:55:43 +0900

    Create Row.tsx

---
+++ src/components/Row.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {
+  title: string;
+  fetchUrl: string;
+  isLargeRow?: boolean;
+};
+
+export const Row = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
