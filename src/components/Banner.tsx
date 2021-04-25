46d8003b300a9b231bd4da3a6a98942846778027
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-25 19:29:12 +0900

    Create Banner.tsx

---
+++ src/components/Banner.tsx
@@ -0,0 +1,7 @@
+import { request } from "https";
+import React, { useState, useEffect } from "react";
+import { DEFAULT_MIN_VERSION } from "tls";
+import axios from "./../axios";
+import { requests } from "./../request";
+// import "./Banner.scss";
+
