5bb8a7f55d469442dcfd7f1d6f5abfe34f8f0a9c
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-29 21:19:44 +0900

    banner_added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { requests } from './request';
 import { Row } from './components/Row';
+import { Banner } from "./components/Banner";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
+      <Banner />
       <Row
         title='Netflix Originials'
         fetchUrl={requests.fetchNetflixOriginals}
