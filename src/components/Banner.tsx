76424bf2c30dd600c38786871c755c765a8cc356
swata-dev <74521093+swata-dev@users.noreply.github.com>
2021-04-29 21:16:07 +0900

    banner_organized

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,20 +1,18 @@
-import { request } from "https";
-import React, { useState, useEffect } from "react";
-import { DEFAULT_MIN_VERSION } from "tls";
+import { useState, useEffect } from "react";
 import axios from "./../axios";
 import { requests } from "./../request";
-// import "./Banner.scss";
+import "./Banner.scss";
 
-type Props = {
+type movieProps = {
   title?: string;
   name?: string;
-  original_name?: string;
+  orignal_name?: string;
   backdrop_path?: string;
   overview?: string;
-}
+};
 
 export const Banner = () => {
-  const [movie, setMovie] = useState<Props>({});
+  const [movie, setMovie] = useState<movieProps>({});
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(requests.fetchNetflixOriginals);
@@ -31,4 +29,37 @@ export const Banner = () => {
     fetchData();
   }, []);
   console.log(movie);
-}
+
+  // descriptionの切り捨てよう関数
+  function truncate(str: any, n: number) {
+    // undefinedを弾く
+    if (str !== undefined) {
+      return str.length > n ? str?.substr(0, n - 1) + "..." : str;
+    }
+  }
+
+  return (
+    <header
+      className="Banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="Banner-contents">
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.orignal_name}
+        </h1>
+        <div className="Banner-buttons">
+          <button className="Banner-button">Play</button>
+          <button className="Banner-button">My List</button>
+        </div>
+
+        <h1 className="Banner-description">{truncate(movie?.overview, 150)}</h1>
+      </div>
+
+      <div className="Banner-fadeBottom" />
+    </header>
+  );
+};
